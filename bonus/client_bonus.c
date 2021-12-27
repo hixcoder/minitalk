befca6c362f61a5f50520438baf9dc634f163b9b
hixcoder <hixcoder@gmail.com>
2021-12-27 03:12:28 +0100

    minitalk_v5

---
+++ bonus/client_bonus.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboumahd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/23 11:44:15 by hboumahd          #+#    #+#             */
+/*   Updated: 2021/12/23 11:44:17 by hboumahd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minitalk.h"
+// SIGUSR1 ==> 1
+// SIGUSR2 ==> 0
+
+void	ft_send_binary_to_server(char *text, int pid)
+{
+	int		i;
+	int		bit_shift;
+
+	i = -1;
+	while (text[++i])
+	{
+		bit_shift = -1;
+		while (++bit_shift < 8)
+		{
+			if (text[i] & 128 >> bit_shift)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(800);
+		}
+	}
+	i = 8;
+	while (i > 0)
+	{
+		kill(pid, SIGUSR2);
+		usleep(500);
+		i--;
+	}
+}
+
+void	ft_signal_handler(int signal)
+{
+	(void)signal;
+	ft_printf("\n==> the server received the message :)\n\n");
+}
+
+int	main(int ac, char **av)
+{
+	int		pid;
+
+	if (ac == 3)
+	{
+		signal(SIGUSR1, ft_signal_handler);
+		pid = ft_atoi(av[1]);
+		if (pid < 0)
+			printf("Please enter a valid PID.");
+		else
+			ft_send_binary_to_server(av[2], pid);
+	}
+	else
+		printf("Please enter the \"PID\" and \"the text\"!\n");
+	return (0);
+}
---
+++ bonus/server_bonus.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboumahd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/23 11:44:24 by hboumahd          #+#    #+#             */
+/*   Updated: 2021/12/23 11:44:25 by hboumahd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minitalk.h"
+// SIGUSR1 ==> 1
+// SIGUSR2 ==> 0
+
+void	ft_handel_error(int pid, int *bit, int *binary)
+{
+	static int	pid_save = 0;
+
+	if (pid_save == 0)
+		pid_save = pid;
+	else if (pid_save != pid)
+	{
+		pid_save = 0;
+		*bit = 0;
+		*binary = 0;
+	}
+}
+
+void	ft_signal_handler(int signal, siginfo_t *info, void *nothing)
+{
+	static int	binary = 0;
+	static int	bit = 0;
+
+	ft_handel_error(info->si_pid, &bit, &binary);
+	bit++;
+	(void)nothing;
+	if (signal == SIGUSR1)
+		binary = binary * 10 + 1;
+	else if (signal == SIGUSR2)
+		binary = binary * 10;
+	if (bit == 8)
+	{
+		ft_printf("%c", ft_binary_to_decimal(binary));
+		if (ft_binary_to_decimal(binary) == 0)
+			kill(info->si_pid, SIGUSR1);
+		bit = 0;
+		binary = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	new_action;
+
+	ft_printf("PID: %d\n", getpid());
+	new_action.sa_sigaction = ft_signal_handler;
+	new_action.sa_flags = SA_SIGINFO;
+	while (1)
+	{
+		sigaction(SIGUSR1, &new_action, NULL);
+		sigaction(SIGUSR2, &new_action, NULL);
+		pause();
+	}
+}
